81cb613ff6ae0d9c3f65a666713fd0d36acc5d58
Veytrinanjunita <bulanveytrinanjunita@gmail.com>
2026-06-09 10:44:35 +0700

    class node public

---
+++ searchtree.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class node
+{
+public:
+    string info;
+    node *leftchild;
+    node *rightchild;
+
+    // Constructor for the node class
+    node(string i, node *l, node *r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
+};

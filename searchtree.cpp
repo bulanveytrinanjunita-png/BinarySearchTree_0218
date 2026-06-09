bac7e56a175812518827e24f32fc3171808894b4
Veytrinanjunita <bulanveytrinanjunita@gmail.com>
2026-06-09 10:58:26 +0700

    class public  binarytree

--- searchtree.cpp
+++ searchtree.cpp
@@ -2,15 +2,15 @@
 #include <string>
 using namespace std;
 
-class node
+class Node
 {
 public:
     string info;
-    node *leftchild;
-    node *rightchild;
+    Node *leftchild;
+    Node *rightchild;
 
     // Constructor for the node class
-    node(string i, node *l, node *r)
+    Node(string i, Node *l, Node *r)
     {
         info = i;
         leftchild = l;
@@ -21,10 +21,40 @@ public:
 class BinaryTree
 {
 public:
-    node *ROOT;
+    Node *ROOT;
 
     BinaryTree()
     {
-        ROOT = nullptr; // Initialize ROOT to null
+        ROOT = NULL; // Initialize ROOT to null
+    }
+
+    // Insert a node in the binary search tree
+    void insert(string element)
+    {
+        Node *newNode = new Node(element, NULL, NULL);
+
+        newNode->info = element;
+        newNode->leftchild = NULL;
+        newNode->rightchild = NULL;
+
+        Node *parent = NULL;
+        Node *currentNode = NULL;
+
+        search(element, parent, currentNode);
+
+        if (parent == NULL)
+        {
+            ROOT = newNode;
+            return;
+        }
+
+        if (element < parent->info)
+        {
+            parent->leftchild = newNode;
+        }
+        else if (element > parent->info)
+        {
+            parent->rightchild = newNode;
+        }
     }
 }
